34cce9e4da016ead00d73df3087955d0acfab2b4
Piotr Michna <pm@piotrmichna.pl>
2020-11-21 23:36:06 +0100

    working on backend aplication

--- guess_the_number3.py
+++ guess_the_number3.py
@@ -6,9 +6,26 @@ app = Flask('GuessTheNumber')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('gues_the_number3.html')
+        min_number = 0
+        max_number = 1000
+        return render_template('gues_the_number3.html', min_number=min_number, max_number=max_number)
     else:
-        return render_template('gues_the_number3.html')
+        min_number = int(request.form["min_number"])
+        max_number = int(request.form["max_number"])
+
+        if request.form["answer"] == "To small" or request.form["answer"] == "To big":
+            if request.form["answer"] == "To small":
+                min_number = int(request.form["guess"])
+            elif request.form["answer"] == "To big":
+                max_number = int(request.form["guess"])
+
+            guess = int((max_number - min_number) / 2 + min_number)
+            return render_template('guess_the_number3_game.html', guess=guess, min_number=min_number, max_number=max_number)
+        else:
+            if request.form["answer"] == "You win":
+                pass
+
+
 
 
 app.run(debug=True)

46f39d09a7a7a00a7922f886c25eb33aa1a69920
Piotr Michna <pm@piotrmichna.pl>
2020-11-21 23:57:00 +0100

    test and format code

--- guess_the_number3.py
+++ guess_the_number3.py
@@ -13,19 +13,19 @@ def index():
         min_number = int(request.form["min_number"])
         max_number = int(request.form["max_number"])
 
-        if request.form["answer"] == "To small" or request.form["answer"] == "To big":
+        if request.form["answer"] != "You win":
             if request.form["answer"] == "To small":
                 min_number = int(request.form["guess"])
             elif request.form["answer"] == "To big":
                 max_number = int(request.form["guess"])
 
             guess = int((max_number - min_number) / 2 + min_number)
-            return render_template('guess_the_number3_game.html', guess=guess, min_number=min_number, max_number=max_number)
+            return render_template('guess_the_number3_game.html', guess=guess, min_number=min_number,
+                                   max_number=max_number)
         else:
             if request.form["answer"] == "You win":
-                pass
-
-
+                end_txt = f'Wygrałem! Twoja liczba to {request.form["guess"]}'
+                return render_template('guess_the_number3_end.html', end_txt=end_txt)
 
 
 app.run(debug=True)
